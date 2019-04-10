dac162f1444ebd61d347fb8955ba36f64bd72476
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-04-10 10:09:18 +0200

    add other missing space

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-check_date <- function(string){
+check_date <- function(string) {
   if(is.na(as.Date(string, format = "%Y-%m-%d"))){
     stop(paste0("The date '", string, "' is not yyyy-mm-dd formatted."),
          call. = FALSE)
